a439e0632c8488c52cb19c5911d7a89440934e34
Eddie <AUNGKAUNG.24@ichat.sp.edu.sg>
2025-05-08 03:18:06 +0800

    Changed it to be an actual countdown and not just going up to 6

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 print("Hello.py")
 for i in range(6):
-    print(i)
-    print("Happy, new year!(Well, not actually)")
-    print("Haha")
+    print(6-i)
+    if i < 7:
+        print("Still not 0 yet!")
+print("Happy, new year!(Well, not actually)")
+print("Haha")
+    
